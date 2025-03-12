47c4f50dc21da0ae3bf5292ca4b9d17cc94abaae
WizzardMaker <wizzardmaker@gmail.com>
2025-03-12 15:38:40 +0100

    added effects to texture component

--- Rendering/Components/TextureComponentRenderer.cs
+++ Rendering/Components/TextureComponentRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using Forge.SDLBackend.Rendering.Textures;
+using Forge.UX.Rendering;
 using Forge.UX.UI;
 using Forge.UX.UI.Components;
 using Forge.UX.UI.Elements;
@@ -31,6 +32,22 @@ internal unsafe class TextureComponentRenderer : IUXComponent {
 
         Texture texture = Component.Texture as Texture ?? throw new InvalidOperationException();
 
+        bool hasEffect = true;
+
+        if (Component.Effects.HasFlag(Effects.Highlight)) {
+            float scale = 1.2f;
+            SDL3.SDL_SetTextureColorModFloat(texture.SDLTexture, scale, scale, scale);
+        }
+
+        if (Component.Effects.HasFlag(Effects.GrayScale)) {
+            float scale = 0.5f;
+            SDL3.SDL_SetTextureColorModFloat(texture.SDLTexture, scale, scale, scale);
+        }
+
         SDL3.SDL_RenderTexture(renderer, texture.SDLTexture, null, &destination);
+
+        if (Component.Effects != Effects.None) {
+            SDL3.SDL_SetTextureColorModFloat(texture.SDLTexture, 1, 1, 1);
+        }
     }
 }
