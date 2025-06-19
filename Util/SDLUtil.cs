a956981622316a30b93753fee53449d9b3df3ee7
WizzardMaker <wizzardmaker@gmail.com>
2025-06-20 00:28:41 +0200

    added error logging to non debug build

--- Util/SDLUtil.cs
+++ Util/SDLUtil.cs
@@ -9,7 +9,6 @@ using System.Runtime.CompilerServices;
 namespace Forge.SDLBackend.Util {
     internal static class SDLUtil {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Conditional("DEBUG")]
         public static void HandleSDLError(bool success, string? message = null) {
             if (!success) {
                 LogSDLError(message);
@@ -17,7 +16,6 @@ namespace Forge.SDLBackend.Util {
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Conditional("DEBUG")]
         public static void LogSDLError(string? message) {
             string? sdlGetError = SDL3.SDL_GetError();
             if (!string.IsNullOrEmpty(sdlGetError)) {
